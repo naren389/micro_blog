eb16aa0d4718ee82fc13cb9d039dce1fe20baff8
narendra <naren389@gmail.com>
2014-01-31 21:36:57 +0530

    Added static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Initialize the page title
+	def full_title(page_title)
+		base_title = "Micro Blog";
+		if page_title.empty?
+			base_title;
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end 
+		
 end
