757c7aed5e8bca7255a09d2b37bc9caa2f98fe14
narendra <naren389@gmail.com>
2014-02-06 11:02:50 +0530

    Completed User model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			# sign the user in and redirect to the user show page
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else
 			# Create an error message and re-render the sign in form
 			flash.now[:error] = "Invalid Email/Password combination"
@@ -18,5 +18,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
+		sign_out
+		redirect_to root_path
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
-
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+
   def new
 	@user = User.new
   end
-  
+
+  def index
+	@users = User.paginate(page: params[:page])
+	#@users = User.all
+  end
+
   def create
 	@user = User.new(params[:user])
 	if @user.save
@@ -14,9 +22,43 @@ class UsersController < ApplicationController
 		render 'new'
 	end
   end	
-	
+
+  def update 
+	@user = User.find(params[:id])
+	if @user.update_attributes(params[:user])
+      # Handle a succesful update	  
+	  flash[:success] = "Profile Update"
+	  sign_in @user
+	  redirect_to @user
+	else
+		render 'edit'
+	end
+  end	
  
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+	@user = User.find(params[:id])
+  end		
+
+  private
+    def signed_in_user
+	  unless signed_in?
+		store_location
+        redirect_to signin_path, notice: "Please Sign In"
+      end
+    end
+
+    def correct_user
+	  @user = User.find(params[:id])
+	  redirect_to(root_path) unless current_user?(@user)	
+	end
+
+	def admin_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user.admin?
+	end
+  
 end
